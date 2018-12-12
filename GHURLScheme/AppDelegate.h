b5286864f2964d8f6946af0d525159bf64f3ddff
shabake <45329453@qq.com>
2018-12-12 10:05:24 +0800

    初始化项目

---
+++ GHURLScheme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHURLScheme
+//
+//  Created by zhaozhiwei on 2018/12/5.
+//  Copyright © 2018年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHURLScheme/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GHURLScheme
+//
+//  Created by zhaozhiwei on 2018/12/5.
+//  Copyright © 2018年 GHome. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
